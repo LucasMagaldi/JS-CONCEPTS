cb2af6cbc2f6ee0f76633581c02b500f13742bf6
Lucas Magaldi <lucas.magaldi@hotmail.com>
2026-08-12 10:50:10 -0300

    chore: promises introduction

---
+++ promises.js
@@ -0,0 +1,9 @@
+/**
+    Promises were introduced in ES6 to provide a cleaner way to manage asyncronous code.
+    Before promises, Javascript handles asynchonous operations using callbacks
+
+    *** What is promises ? ***
+     An object that represents the result of an asynchronous operation.
+ */
+
+
